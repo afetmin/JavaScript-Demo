97f67ab2ae29d21b709069f0de150839922c2883
afetmin <kingpenguins@163.com>
2021-01-10 15:24:38 +0800

    typescript

--- 函数柯里化.js
+++ 函数柯里化.js
@@ -1,17 +1,38 @@
-function add() {
-  let args = [...arguments] || [].slice.call(arguments)
-  // 递归把所有的参数存到args里面
-  let _adder = (...newArgs) => {
-    return add(...[...args, ...newArgs])
-  }
-  _adder.toString = () => {
-    return args.reduce((a, b) => {
-      return a + b
-    })
-  }
+// function add() {
+//   let args = [...arguments] || [].slice.call(arguments)
+//   // 递归把所有的参数存到args里面
+//   let _adder = (...newArgs) => {
+//     return add(...[...args, ...newArgs])
+//   }
+//   _adder.toString = () => {
+//     return args.reduce((a, b) => {
+//       return a + b
+//     })
+//   }
 
-  return _adder
+//   return _adder
+// }
+
+function currying(fn) {
+  var allArgs = [];
+
+  return function next() {
+    var args = [].slice.call(arguments);
+
+    if (args.length > 0) {
+      allArgs = allArgs.concat(args);
+      return next;
+    } else {
+      return fn.apply(null, allArgs);
+    }
+  }
 }
+let add = currying(function () {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+});
 
-console.log(add(1)(2)(3).toString())
-console.log(add(1, 2, 3).toString())
+add(1)(2,3)()
