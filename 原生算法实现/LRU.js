4f978961659ef0ba10fc34727b5574d0e14745c0
afetmin <kingpenguins@163.com>
2021-04-01 10:40:02 +0800

    LRU

---
+++ 原生算法实现/LRU.js
@@ -0,0 +1,24 @@
+class LRUcache {
+  constructor(n) {
+    this.size = n
+    this.data = new Map()
+  }
+
+  set(domain, info) {
+    if (this.data.size >= this.size) {
+      const firstKey = [...this.data.keys()][0]
+      this.data.delete(firstKey)
+    }
+    this.data.set(domain, info)
+  }
+
+  get(domain) {
+    if (!this.data.has(domain)) {
+      return false
+    }
+    const info = this.data.get(domain)
+    this.data.delete(domain)
+    this.data.set(domain, info)
+    return info
+  }
+}
