b65a911264e6ac474b81d662e93820a46231f404
afetmin <kingpenguins@163.com>
2021-07-18 08:52:51 +0800

    feat: 洗牌算法

---
+++ suffle.js
@@ -0,0 +1,21 @@
+const nums = Array(54)
+  .fill(undefined)
+  .map((_, index) => index);
+
+/**
+ * 洗牌算法
+ * @param nums 数组
+ */
+function FYSufffle(nums) {
+  const randNums = Array.from(nums);
+  let len = nums.length;
+  while (len > 1) {
+    const rand = Math.floor(Math.random() * len);
+    len--;
+    // 交换
+    [randNums[rand], randNums[len]] = [randNums[len], randNums[rand]];
+  }
+  return randNums;
+}
+
+console.log(FYSufffle(nums));
