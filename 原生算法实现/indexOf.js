de54ce212f6f56919f4a002c8e8cccdf1c1bae6a
afetmin <kingpenguins@163.com>
2021-09-09 20:05:27 +0800

    String.prototype.indexOf

--- 原生算法实现/indexOf.js
+++ 原生算法实现/indexOf.js
@@ -1,9 +1,14 @@
 String.prototype._indexof = function (s) {
   for (var i = 0; i < this.length - s.length; i++) {
-    console.log(this.charAt(i), this.substring(i))
-    if (this.charAt(i) === s.charAt(0) && this.substring(i, s.length) === s) {
+    if (
+      this.charAt(i) === s.charAt(0) &&
+      this.substring(i, i + s.length) === s
+    ) {
       return i
     }
   }
   return -1
 }
+
+const s = "hello"
+console.log(s._indexof("ll"))
