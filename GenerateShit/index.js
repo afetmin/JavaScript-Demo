a09aee6e697ca3074a640a563113097cb806d61c
afetmin <kingpenguins@163.com>
2022-10-31 08:40:56 +0800

    interact

--- GenerateShit/index.js
+++ GenerateShit/index.js
@@ -2,10 +2,25 @@ import { createRandomPicker } from './lib/random.js';
 import { generate } from './lib/generator.js';
 import { loadCorpus, saveArticle } from './lib/corpus.js';
 import { options } from './lib/cmd.js';
+import { interact } from './lib/interact.js';
+
 
 
 const corpus = loadCorpus('./corpus/data.json');
-const title = options.title || createRandomPicker(corpus['title'])();
+
+let title = options.title || createRandomPicker(corpus['title'])();
+
+const questions = [
+    { text: '请输入文章主题', value: title },
+    { text: '请输入最小字数', value: 6000 },
+    { text: '请输入最大字数', value: 10000 },
+];
+
+const answers = await interact(questions);
+title = answers[0]
+options.min = answers[1];
+options.max = answers[2];
+
 const article = generate(title, { corpus, ...options });
 const output = saveArticle(title, article);
 console.log(`success: ${output}`);
